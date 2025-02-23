ad0b1f08555a3f59a45b4f36f082592b47abb635
Artem <artemzlobenko@gmail.com>
2025-02-23 21:20:54 +0200

    refactor: delete unused file

--- master.py
+++
@@ -1,6 +0,0 @@
-from time import sleep
-from pymodbus.client import ModbusTcpClient
-with ModbusTcpClient(host='127.0.0.1', port=502) as client:
-    while True:
-        result = client.read_holding_registers(address=0, count=10, slave=1)
-        print(result.getRegister(0))
